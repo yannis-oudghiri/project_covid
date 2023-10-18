5c1c12ac78ff8a1ed4f2b6ffc661ff4bea084ed5
ajinkya0919 <bhaleraoajinkya878@gmail.com>
2023-10-18 11:45:40 +0200

    minor edits

--- app.py
+++ app.py
@@ -31,26 +31,39 @@ mendeley_grouped = mendeley.groupby(by = "location").agg(np.mean)
 mendeley_grouped.reset_index(inplace=True)
 
 mendeley_grouped["continent"] = continent_code
-df2_grouped = mendeley_grouped.replace({'AS':'Asia', 'EU':'Europe', 'AF':'Africa', 'AN':'Antarctica', 'NA':'North America', 'SA':'South America', 'OC':'Oceania'})
+mendeley_grouped = mendeley_grouped.replace({'AS':'Asia', 'EU':'Europe', 'AF':'Africa', 'AN':'Antarctica', 'NA':'North America', 'SA':'South America', 'OC':'Oceania'})
 
 europe_growth_stack = europe_growth.stack().reset_index().rename(columns = {'level_0' : 'date','level_1' : 'Country', 0 : 'growth'})
 
 fig1 = px.line(europe_growth_stack, x='date', y='growth', color='Country', title='GROWTH')
-#fig1.show()
 
 fig2 = px.scatter(mendeley_grouped, x="human_development_index", y="gdp_per_capita", size="population", hover_name="location", color='location', template='simple_white', size_max=70)
 fig2.update_layout(
     height=500,
     title_text="Comparison between a Country's GDP per Capita and HDI"
 )
-#fig2.show()
+
+fig3 = px.scatter(mendeley_grouped, x="gdp_per_capita", y="total_cases", color="location" ,size="population", hover_name="location", template='simple_white', size_max=70)
+fig3.update_layout(
+    height=500,
+    title_text="COVID-19 Cases (mean) vs GDP per Capita (per Country)"
+)
+
+fig4 = px.bar(mendeley_grouped, x='continent', y='total_cases', hover_name='location', color='continent', template="simple_white")
+fig4.update_layout(
+    height=500,
+    title_text='COVID-19 Cases per Continent'
+)
+fig4.update_xaxes(showticklabels=False)
 
 app = Dash(__name__)
 
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Graph(figure=fig1),
-    dcc.Graph(figure=fig2)
+    dcc.Graph(figure=fig2),
+    dcc.Graph(figure=fig3),
+    dcc.Graph(figure=fig4)
 ])
 
 if __name__ == '__main__':
